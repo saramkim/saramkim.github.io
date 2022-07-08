0aaa7cbcf72594dadf7d209567c0ea3becc116cb
saramkim <yujun1018@naver.com>
2022-07-08 21:44:06 +0900

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `주니어 개발자의 개발 블로그`,
     description: `주니어 개발자로서의 저를 표현한 블로그입니다.`,
     author: `Saram`,
-    siteUrl: 'https://my-website-link.com', // 배포 후 변경 예정
+    siteUrl: 'https://saramkim.github.io/', // 배포 후 변경 예정
   },
   plugins: [
     {
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: '<https://my-website.com/>',
+        siteUrl: 'https://saramkim.github.io/',
         stripQueryString: true,
       },
     },
