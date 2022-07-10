8a148c452e959e44b998efb6cc1e74cd75b7ee82
saram <yujun1018@naver.com>
2022-07-10 13:29:26 +0900

    change name

--- src/components/Main/Introduction.tsx
+++ src/components/Main/Introduction.tsx
@@ -58,7 +58,7 @@ const Introduction: FunctionComponent<IntroductionProps> = function ({
 
         <div>
           <SubTitle>Nice to Meet You,</SubTitle>
-          <Title>I'm Junior Frontend Developer Hyun.</Title>
+          <Title>I'm Junior Frontend Developer Saram</Title>
         </div>
       </Wrapper>
     </Background>
